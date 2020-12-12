bdb1d355044bd00feb69af409a45efd0a0c5a4c2
MahmoudSiliman <modysiliman75@gmail.com>
2020-12-12 14:22:54 +0200

    edit player details page

--- player-details.php
+++ player-details.php
@@ -4,82 +4,84 @@
 
 <!-- START:: CONTENT -->
 <div class="player-details">
-  <div class="container">
-    <h3 class="mb-4"> محمد الشناوى  <span> (حارس مرمى) </span> </h3>
+  <div class="overlay">
+    <div class="container">
+      <h3 class="mb-4"> محمد الشناوى  <span> (حارس مرمى) </span> </h3>
 
-    <div class="row m-0">
+      <div class="row m-0">
 
-      <!-- START:: PLAYER DATA SECTION -->
-      <div class="row col-12  col-md-6 justify-content-center align-items-center mb-3">
+        <!-- START:: PLAYER DATA SECTION -->
+        <div class="row col-12  col-md-6 justify-content-center align-items-center mb-3">
 
-        <div class="player-img col-12 text-center mb-4">
-          <div class="img-border">
-            <img src="imgs/player.png" class="img-fluid" alt="">  
+          <div class="player-img col-12 text-center mb-4">
+            <div class="img-border">
+              <img src="imgs/player.png" class="img-fluid" alt="">  
+            </div>
           </div>
         </div>
+        <!-- END:: PLAYER DATA SECTION -->
+
+        <!-- START:: PLAYER INFO SECTION -->
+        <div class="row col-12 col-md-6">
+
+        
+          <div class="col-12 player-info mb-3">
+            <p> <span> النادى :  </span> الأهلى </p>
+            <p> <span> الجنسية :  </span> مصرى </p>
+            <p> <span> السن :  </span> 31 </p>
+            <p> <span> رقم القميص :  </span> 1 </p>
+          </div>
+
+          <!-- START:: STATISTIC CARD -->
+          <div class="col-6 p-2">
+            <div class="card play">
+              <div class="card-body text-center">
+                <h4> لعب </h4>
+                <p> 9 </p>
+              </div>
+            </div>
+          </div>
+          <!-- END:: STATISTIC CARD -->
+
+          <!-- START:: STATISTIC CARD -->
+          <div class="col-6 p-2">
+            <div class="card goals">
+              <div class="card-body text-center">
+                <h4> شباك نظيفة </h4>
+                <p> 8 </p>
+              </div>
+            </div>
+          </div>
+          <!-- END:: STATISTIC CARD -->
+
+          <!-- START:: STATISTIC CARD -->
+          <div class="col-6 p-2">
+            <div class="card yellow-cards">
+              <div class="card-body text-center">
+                <h4> كروت صفراء </h4>
+                <p> 0 </p>
+              </div>
+            </div>
+          </div>
+          <!-- END:: STATISTIC CARD -->
+
+          <!-- START:: STATISTIC CARD -->
+          <div class="col-6 p-2">
+            <div class="card red-cards">
+              <div class="card-body text-center">
+                <h4> كروت حمراء </h4>
+                <p> 0 </p>
+              </div>
+            </div>
+          </div>
+          <!-- END:: STATISTIC CARD -->
+
+        </div>
+        <!-- END:: PLAYER INFO SECTION -->
+
       </div>
-      <!-- END:: PLAYER DATA SECTION -->
-
-      <!-- START:: PLAYER INFO SECTION -->
-      <div class="row col-12 col-md-6">
-
-      
-        <div class="col-12 player-info mb-3">
-          <p> <span> النادى :  </span> الأهلى </p>
-          <p> <span> الجنسية :  </span> مصرى </p>
-          <p> <span> السن :  </span> 31 </p>
-          <p> <span> رقم القميص :  </span> 1 </p>
-        </div>
-
-        <!-- START:: STATISTIC CARD -->
-        <div class="col-6 p-2">
-          <div class="card play">
-            <div class="card-body text-center">
-              <h4> لعب </h4>
-              <p> 9 </p>
-            </div>
-          </div>
-        </div>
-        <!-- END:: STATISTIC CARD -->
-
-        <!-- START:: STATISTIC CARD -->
-        <div class="col-6 p-2">
-          <div class="card goals">
-            <div class="card-body text-center">
-              <h4> شباك نظيفة </h4>
-              <p> 8 </p>
-            </div>
-          </div>
-        </div>
-        <!-- END:: STATISTIC CARD -->
-
-        <!-- START:: STATISTIC CARD -->
-        <div class="col-6 p-2">
-          <div class="card yellow-cards">
-            <div class="card-body text-center">
-              <h4> كروت صفراء </h4>
-              <p> 0 </p>
-            </div>
-          </div>
-        </div>
-        <!-- END:: STATISTIC CARD -->
-
-        <!-- START:: STATISTIC CARD -->
-        <div class="col-6 p-2">
-          <div class="card red-cards">
-            <div class="card-body text-center">
-              <h4> كروت حمراء </h4>
-              <p> 0 </p>
-            </div>
-          </div>
-        </div>
-        <!-- END:: STATISTIC CARD -->
-
-      </div>
-      <!-- END:: PLAYER INFO SECTION -->
 
     </div>
-
   </div>
 </div>
 <!-- END:: CONTENT -->
